04ceaa6269e7debb434f5278864a53115ba6532c
kshipradewat <kshipradewat66@gmail.com>
2025-03-06 17:46:02 +0530

    fixing

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,10 +1,9 @@
-
 "use client";
 
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import Profile from "@components/Profile";
 
+import Profile from "@components/Profile";
 
 const UserProfile = ({ params }) => {
   const searchParams = useSearchParams();
