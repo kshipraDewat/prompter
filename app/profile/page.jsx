0898d9d1b246a18b8bb187c057d56b6712334e6c
kshipradewat <kshipradewat66@gmail.com>
2025-03-06 18:12:39 +0530

    fixing 3

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,7 +5,7 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
-import Profile from "@components/Profile.jsx";
+import Profile from "@components/Profile";
 
 const MyProfile = () => {
   const router = useRouter();
